001836440fea6d1f24f7b3c026c0727552bf9028
ikaros <1371689491@qq.com>
2017-09-10 16:33:54 +0800

    04

--- wikiHistories.py
+++ wikiHistories.py
@@ -1,12 +1,14 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import datetime
 import random
 import re
+import json
 
 random.seed(datetime.datetime.now())
 
-def getLinks(articeUrl):
+def getLinks(articleUrl):
     html = urlopen('http://en.wikipedia.org'+articleUrl)
     bsObj = BeautifulSoup(html, 'html.parser')
     return bsObj.find('div', {'id': 'bodyContent'}).findAll('a', href=re.compile('^(/wiki/)((?!:).)*$'))
@@ -18,7 +20,7 @@ def getHistoryIps(pageUrl):
     historyUrl = 'http://en.wikipedia.org/w/index.php?title='+pageUrl+'&action=history'
     print('history url is: '+historyUrl)
     html = urlopen(historyUrl)
-    bsObj = BeautifulSoup(html, 'html.patser')
+    bsObj = BeautifulSoup(html, 'html.parser')
     # 找出class属性是"mw-anonuserlink"的链接
     # 它们用IP地址代替用户名
     ipAddresses = bsObj.findAll('a', {'class': 'nw-anonuserlink'})
@@ -27,6 +29,14 @@ def getHistoryIps(pageUrl):
         addressList.add(ipAddress.get_text())
     return addressList
 
+def getCountry(ipAddress):
+    try:
+        response = urlopen('http://freegeoip.net/json/'+ipAddress).read().decode('utf-8')
+    except HTTPError:
+        return None
+    responseJson = json.loads(response)
+    return responseJson.get('country_code')
+
 links = getLinks('/wiki/Python_(programming_language)')
 
 while(len(links) > 0):
@@ -34,7 +44,9 @@ while(len(links) > 0):
         print('-----------')
         historyIPs = getHistoryIps(link.attrs['href'])
         for historyIP in historyIPs:
-            print(historyIP)
+            country = getCountry(historyIP)
+            if country is not None:
+                print(historyIP+' is from '+country)
 
     newLink = links[random.randint(0, len(links)-1)].attrs['href']
     links = getLinks(newLink)
